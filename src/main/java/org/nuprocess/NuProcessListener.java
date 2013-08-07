12bada36200354b613dd33b33cd83d4f779cef0a
Brett Wooldridge <brett.wooldridge@gmail.com>
2013-08-07 17:09:14 +0900

    Add javadoc.

--- src/main/java/org/nuprocess/NuProcessListener.java
+++ src/main/java/org/nuprocess/NuProcessListener.java
@@ -15,6 +15,9 @@ public interface NuProcessListener
      * completely process all of the contents of the buffer when it is
      * passed to you</i>, as it's contents will be overwritten immediately
      * upon receipt of more data.
+     * <p>
+     * Exceptions thrown out from your method will be ignored, but your
+     * method should handle all exceptions itself.
      *
      * @param buffer a ByteBuffer containing received stdout data, or null
      *               signifying that an EOF condition has been reached
@@ -34,8 +37,21 @@ public interface NuProcessListener
      * upon receipt of more data.
      *
      * User wishing to merge stderr into stdout should simply delegate
-     * this callback to <code>onStdout(buffer)</code> when invoked.
-     *
+     * this callback to <code>onStdout(buffer)</code> when invoked, like so:
+     * <pre>
+     *    public void onStderr(ByteBuffer buffer) {
+     *       if (buffer != null) {
+     *          onStdout(buffer);
+     *       }
+     *    }
+     * </pre>
+     * Notice that a null check is performed.  If you merge streams in
+     * this way, and you do not check for null here, then your onStdout()
+     * method will be called twice for an EOF condition; once when the
+     * stdout stream closes, and once when the stderr stream closes.  If
+     * you check for null as above, your onStdout() method would only
+     * be called once (for the close of stdout).
+     * 
      * @param buffer a ByteBuffer containing received stderr data, or null
      *               signifying that an EOF condition has been reached
      */
