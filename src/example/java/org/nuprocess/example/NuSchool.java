429c83086e16ab3bd458ae5cec38e468301bf0e9
brettwooldridge <brett.wooldridge@gmail.com>
2013-09-12 12:35:37 +0900

    Fix path for windows.

--- src/example/java/org/nuprocess/example/NuSchool.java
+++ src/example/java/org/nuprocess/example/NuSchool.java
@@ -27,7 +27,7 @@ public class NuSchool
         String command = "/bin/cat";
         if (System.getProperty("os.name").toLowerCase().contains("win"))
         {
-            command = "src\\test\\java\\org\\nuprocess\\windows\\cat.exe";
+            command = "src\\test\\java\\org\\nuprocess\\cat.exe";
         }
 
         NuProcessBuilder pb = new NuProcessBuilder(Arrays.asList(command));
