7c266e236b80cbd40ea1253054b486f24f373420
Bryan Turner <bturner@atlassian.com>
2018-07-15 12:11:53 -0700

    Use ALIGN_NONE only on x86-64.
    - Added EpollEvent.EpollEventPrototype.detectAlignment(), which uses
      JNA's Platform utilities to detect x86-64 and use ALIGN_NONE
      - On any other architecture, ALIGN_GNUC is used
      - When running a 32-bit JVM on a 64-bit OS, ALIGN_GNUC will be used
    - Updated EpollEventTest.testSize to set the expected size to 12 or 16
      bytes depending on the architecture detected by JNA's Platform

--- src/test/java/com/zaxxer/nuprocess/linux/EpollEventTest.java
+++ src/test/java/com/zaxxer/nuprocess/linux/EpollEventTest.java
@@ -1,17 +1,23 @@
 package com.zaxxer.nuprocess.linux;
 
+import com.sun.jna.Platform;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class EpollEventTest
 {
-   // ensure EpollEvent is 12 bytes, to match its size in C
+   // ensure EpollEvent's size matches the platform:
+   // - 12 bytes on all 32-bit architectures (4 byte aligned)
+   // - 12 bytes on x86-64, where it's compiled with __attribute__((packed)) (1 byte aligned)
+   // - 16 bytes on all other 64-bit architectures (8 byte aligned)
    @Test
    public void testSize()
    {
+      // 64-bit architectures use a 16 byte struct, except on AMD/Intel, where the struct is 12 bytes
+      // on both 32- and 64-bit. The struct is 12 bytes on all 32-bit architectures
+      int expectedSize = (Platform.is64Bit() && !Platform.isIntel()) ? 16 : 12;
+
       EpollEvent event = new EpollEvent();
-      Assert.assertEquals(16, event.size());
-      // When alignment is ALIGN_NONE
-      // Assert.assertEquals(12, event.size());
+      Assert.assertEquals(expectedSize, event.size());
    }
 }
